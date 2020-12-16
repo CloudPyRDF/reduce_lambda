33c0ade245fa329d9c99ea1ca4b7e5b4cfec8c81
Jacek Kuśnierz <jaku@grapeup.com>
2020-12-16 03:53:35 +0100

    hmm

--- lambda.py
+++ lambda.py
@@ -30,7 +30,6 @@ def lambda_handler(event, context):
         s3.upload_file(file_paths[0], bucket, 'out.pickle')
         return {'statusCode': 200, 'body': json.dumps(f'Returned merged histogram to {bucket} bucket')}
 
-    import pickle
     print("all files are in place, merging")
 
     pickled_reducer = base64.b64decode(ssm.get_parameter(Name='reducer')['Parameter']['Value'][2:-1])
@@ -39,9 +38,14 @@ def lambda_handler(event, context):
 
     glue = f"""
 def reduce_function(reducer, files):
-    accumulator = reducer(files[0], files[1])
+    file1=pickle.load(open(files[0], 'rb'))
+    file2=pickle.load(open(files[1], 'rb'))
+    accumulator = reducer(file1,file2)
+    print("got this far not dying")
+    
     for file in files[2:]:
-        accumulator = reducer(accumulator, file)
+        file_unpickled=pickle.load(open(file, 'rb'))
+        accumulator = reducer(accumulator, file_unpickled)
     pickle.dump(accumulator, open('/tmp/out.pickle', 'wb'))
 
 import cloudpickle as pickle
@@ -55,12 +59,15 @@ reduce_function(pickle.loads({pickled_reducer}),{file_paths})
     result = os.system('''
         export PATH=/mnt/cern_root/chroot/usr/local/sbin:/mnt/cern_root/chroot/usr/local/bin:/mnt/cern_root/chroot/usr/sbin:/mnt/cern_root/chroot/usr/bin:/mnt/cern_root/chroot/sbin:/mnt/cern_root/chroot/bin:$PATH && \
         export LD_LIBRARY_PATH=/mnt/cern_root/chroot/usr/lib64:/mnt/cern_root/chroot/usr/lib:/usr/lib64:/usr/lib:$LD_LIBRARY_PATH && \
+        export CPATH=/mnt/cern_root/chroot/usr/include:$CPATH && \
         export PYTHONPATH=/mnt/cern_root/root_install/PyRDF:/mnt/cern_root/root_install:$PYTHONPATH && \
-        export CPATH=/mnt/cern_root/chroot/usr/include:$CPATH && \            
         export roothome=/mnt/cern_root/root_install && \
+        cd /mnt/cern_root/root_install/PyRDF && \
         . ${roothome}/bin/thisroot.sh && \
         /mnt/cern_root/chroot/usr/bin/python3.7 /tmp/to_execute.py
     ''')
+    if not result:
+        print("not result", result)
     output_bucket = ssm.get_parameter(Name='output_bucket')['Parameter']['Value']
 
     s3.upload_file(f'/tmp/out.pickle', output_bucket, 'out.pickle')
