36c24b4ba54f50f6fb27eb308fb0fe45cbbfed5c
Jacek Kuśnierz <jaku@grapeup.com>
2020-12-16 03:38:47 +0100

    cmon

--- lambda.py
+++ lambda.py
@@ -12,15 +12,19 @@ def lambda_handler(event, context):
     s3 = boto3.client('s3')
 
     ranges_number = int(ssm.get_parameter(Name='ranges_num')['Parameter']['Value'])
-    files_in_s3 = s3.list_objects_v2(bucket)['KeyCount']
+    files_in_s3 = s3.list_objects_v2(Bucket=bucket)['KeyCount']
     if ranges_number != files_in_s3:
         print(f'{files_in_s3}/{ranges_number} ready')
         return {'statusCode': 400, 'body': json.dumps(f'not yet, only {files_in_s3}/{ranges_number} ready')}
 
+    if not os.path.exists('/tmp/result'):
+        os.makedirs('/tmp/result')
     file_paths = []
-    for s3_object in s3.list_objects_v2(bucket)['Contents']:
-        file_path = f"/tmp/result/{s3_object['key']}"
-        s3.download_file(bucket, s3_object['key'], file_path)
+    for s3_object in s3.list_objects_v2(Bucket=bucket)['Contents']:
+        print(s3_object)
+        file_path = f"/tmp/result/{s3_object['Key']}"
+        file_paths.append(file_path)
+        s3.download_file(bucket, s3_object['Key'], file_path)
 
     if ranges_number == 1:
         s3.upload_file(file_paths[0], bucket, 'out.pickle')
@@ -30,19 +34,18 @@ def lambda_handler(event, context):
     print("all files are in place, merging")
 
     pickled_reducer = base64.b64decode(ssm.get_parameter(Name='reducer')['Parameter']['Value'][2:-1])
-    reducer = pickle.loads(pickled_reducer)
 
-    def reduce_function(reducer, files):
-        accumulator = reducer(files[0], files[1])
-        for file in files[2:]:
-            accumulator = reducer(accumulator, file)
-        pickle.dump(accumulator, open('/tmp/out.pickle', 'w'))
-
-    pickled_script = pickle.dumps(reduce_function)
+    print(file_paths)
 
     glue = f"""
-import pickle
-pickle.loads({pickled_script})({reducer},{file_paths})
+def reduce_function(reducer, files):
+    accumulator = reducer(files[0], files[1])
+    for file in files[2:]:
+        accumulator = reducer(accumulator, file)
+    pickle.dump(accumulator, open('/tmp/out.pickle', 'wb'))
+
+import cloudpickle as pickle
+reduce_function(pickle.loads({pickled_reducer}),{file_paths})
 """
 
     script_file = open('/tmp/to_execute.py', "w")
@@ -52,6 +55,7 @@ pickle.loads({pickled_script})({reducer},{file_paths})
     result = os.system('''
         export PATH=/mnt/cern_root/chroot/usr/local/sbin:/mnt/cern_root/chroot/usr/local/bin:/mnt/cern_root/chroot/usr/sbin:/mnt/cern_root/chroot/usr/bin:/mnt/cern_root/chroot/sbin:/mnt/cern_root/chroot/bin:$PATH && \
         export LD_LIBRARY_PATH=/mnt/cern_root/chroot/usr/lib64:/mnt/cern_root/chroot/usr/lib:/usr/lib64:/usr/lib:$LD_LIBRARY_PATH && \
+        export PYTHONPATH=/mnt/cern_root/root_install/PyRDF:/mnt/cern_root/root_install:$PYTHONPATH && \
         export CPATH=/mnt/cern_root/chroot/usr/include:$CPATH && \            
         export roothome=/mnt/cern_root/root_install && \
         . ${roothome}/bin/thisroot.sh && \
